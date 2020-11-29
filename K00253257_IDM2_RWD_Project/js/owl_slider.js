f94502c03c5750f4c26d452b3a7ffe6ae72efb7f
tomc <tmcgraphic@gmail.com>
2020-11-29 23:06:29 +0000

    Adding project

---
+++ K00253257_IDM2_RWD_Project/js/owl_slider.js
@@ -0,0 +1,9 @@
+$(".slider").owlCarousel({
+ 
+    loop : true,
+    margin: 310,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+# RWD
+web_dev
